6aa103dfbf361bcb39c8c489afe6f8474f9164ee
hiarium <kd1300424@st.kobedenshi.ac.jp>
2021-06-07 18:58:16 +0900

    6/7 commit

--- Bacteria/Assets/Script/Bacteria.cs
+++ Bacteria/Assets/Script/Bacteria.cs
@@ -1,53 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bacteria : MonoBehaviour
 {
-    public float xspeed;
-    public float yspeed;
-    private Rigidbody2D rb;
-    private GameObject gm;
-    private GameObject copy;
+    public float speed;
+    public char state;
+    public Vector2 movepoint;
+    private float time=1;
+    private Coroutine div;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        gm = GameObject.Find("GameManager");
-        copy = (GameObject)Resources.Load("Bacteria");
-        StartCoroutine("Division");
+        state = 'D';
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            xspeed = -1;
-        } else if (Input.GetKey(KeyCode.RightArrow)) {
-            xspeed = 1;
+        // 状態変数によって行動を分岐
+        // 分裂(Division)
+        if(state=='D'){
+            if(div==null){
+                div = StartCoroutine("Division");
+            }
+        }else{
+            if(div!=null){
+                StopCoroutine(div);
+                div = null;
+            }
         }
-        else
-        {
-            xspeed = 0;
+        // 選択(Select)
+        if(state=='S'){
+            this.GetComponent<Image>().color = GetAlphaColor();
+        }else{
+            if(GetComponent<Image>().color.a!=1){
+                GetComponent<Image>().color =new Color(255,255,255,1);
+            }
+            time=1;
         }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            yspeed = 1;
+        // 移動(Move)
+        if(state=='M'){
+            while(movepoint!=new Vector2(0,0)){
+                
+            }
+        }else{
+            
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            yspeed = -1;
+        // 戦闘(Fight)
+        if(state=='F'){
+
+        }else{
+            
         }
-        else
-        {
-            yspeed = 0;
-        }
-        
-        rb.velocity = new Vector2(xspeed, yspeed);
-        
     }
 
     private IEnumerator Division()
@@ -57,11 +64,12 @@ public class Bacteria : MonoBehaviour
         RaycastHit2D hit;
         while (true)
         {
-            yield return new WaitForSeconds(1.0f);
+            yield return new WaitForSeconds(3.0f);
             local = transform.InverseTransformPoint (transform.position);
+            Debug.Log(transform.position);
+            Debug.Log(local);
             direction=transform.TransformPoint (local+new Vector3(1.02f,0,0));
             hit = Physics2D.BoxCast(direction, transform.lossyScale, 0, Vector2.zero);
-            Debug.Log(hit.collider);
             if(hit.collider==null){
                 Instantiate ((GameObject)Resources.Load("Bacteria"), direction, Quaternion.identity,GameObject.Find("Canvas").transform);
             }
@@ -74,5 +82,10 @@ public class Bacteria : MonoBehaviour
         Gizmos.color = Color.red; //色指定
         Gizmos.DrawCube(transform.TransformPoint (local+new Vector3(1.02f,0,0)), transform.lossyScale); //中心点とサイズ
     }
-    
+
+    Color GetAlphaColor() {
+        time += Time.deltaTime * 7.0f;
+        Color color =new Color(255,255,255, Mathf.Sin(time) * 0.5f + 0.5f);
+        return color;
+    }
 }
--- Bacteria/Assets/Script/GameManager.cs
+++ Bacteria/Assets/Script/GameManager.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public int fun_counter = 1;
-    private LineRenderer renderer;
+    private LineRenderer line1;
+    private LineRenderer lRend;
+    private GameObject line2; 
     private PolygonCollider2D collider;
     private Vector3 start_pos;
     private Vector3 before_pos;
+    private bool circle = false;
+    private List<GameObject> selectObject = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        renderer = gameObject.GetComponent<LineRenderer>();
+        line1 = gameObject.GetComponent<LineRenderer>();
         collider = gameObject.GetComponent<PolygonCollider2D>();
     }
 
@@ -21,16 +25,82 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            renderer.positionCount = 1;
             //タッチ位置取得
             Vector3 cameraPosition = Input.mousePosition;
             cameraPosition.z = 10;
             start_pos = Camera.main.ScreenToWorldPoint(cameraPosition);
-            //ラインの起点設定
-            renderer.SetPosition(0, start_pos);
-            before_pos = start_pos;
-            //コルーチンスタート
-            StartCoroutine("Line");
+
+            // 描画されてない時
+            if(!circle){
+                line1.positionCount = 1;
+                //line1の起点設定
+                line1.SetPosition(0, start_pos);
+                before_pos = start_pos;
+                //コルーチンスタート
+                StartCoroutine("Line");
+            }
+            // 描画済の時
+            else{
+                // 円内のクリックか判定
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+                //円内の時
+                if (hit2d.collider&&hit2d.transform.gameObject.name == "GameManager") {
+                    // line2生成
+                    line2 = new GameObject ("Line");
+                    line2.transform.parent = GameObject.Find("Canvas").transform;
+                    lRend = line2.AddComponent<LineRenderer> ();
+                    lRend.SetVertexCount(2);
+                    lRend.SetColors(new Color(0,37,255),new Color(0,0.13f,1));
+                    lRend.material = new Material(Shader.Find("Sprites/Default"));
+                    lRend.SetWidth (0.1f, 0.1f);
+                    lRend.SetPosition (0, start_pos);
+                    lRend.SetPosition (1, start_pos);
+                }
+                // 円外の時
+                else{
+                    // 既存line1破壊
+                    line1.positionCount = 1;
+                    collider.pathCount=0;
+                    circle = false;
+                    line1.loop = false;
+                    while(selectObject.Count!=0){
+                        selectObject[0].GetComponent<Bacteria>().state = 'D';
+                        selectObject.RemoveAt(0);
+                    }
+                    //line1の再描画
+                    line1.SetPosition(0, start_pos);
+                    before_pos = start_pos;
+                    //コルーチンスタート
+                    StartCoroutine("Line");
+                }
+            }
+        }
+        if (Input.GetMouseButton(0)){
+            if(line2 != null){
+                Vector3 cameraPosition = Input.mousePosition;
+                cameraPosition.z = 10;
+                lRend.SetPosition (1, Camera.main.ScreenToWorldPoint(cameraPosition));
+            }
+        }
+        // line2描画済みの時、全line削除と選択中の菌を移動する
+        if (Input.GetMouseButtonUp(0)){
+            if(line2 != null){
+                // 選択中の菌に移動先指定
+                Vector3[] lPos = new Vector3[2];
+                lRend.GetPositions(lPos);
+                while(selectObject.Count!=0){
+                    selectObject[0].GetComponent<Bacteria>().state = 'M';
+                    selectObject[0].GetComponent<Bacteria>().movepoint = lPos[1]-lPos[0];
+                    selectObject.RemoveAt(0);
+                }
+                // 全line削除
+                Destroy(line2);
+                line1.positionCount = 1;
+                collider.pathCount=0;
+                circle = false;
+                line1.loop = false;
+            }
         }
     }
 
@@ -39,48 +109,54 @@ public class GameManager : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(0.01f);
+            // クリック中line描画処理
             if (Input.GetMouseButton(0))
             {
-                if (renderer.positionCount < 50)
+                if (line1.positionCount < 50)
                 {
                     //タッチ位置取得
                     Vector3 cameraPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                     cameraPosition.z = 0;
                     if (before_pos != cameraPosition)
                     {
-                        renderer.positionCount += 1;
-                        renderer.SetPosition(renderer.positionCount - 1, cameraPosition);
+                        line1.positionCount += 1;
+                        line1.SetPosition(line1.positionCount - 1, cameraPosition);
                         before_pos = cameraPosition;
                     }
                 }
             }
-            else
+            else　// クリックをやめたときの処理
             {
-                if (renderer.positionCount < 3)
+                // line頂点の数が3未満ならなかったことにする
+                if (line1.positionCount < 3)
                 {
-                    renderer.positionCount = 0;
-                    break;
+                    line1.positionCount = 0;
+                    yield break;
                 }
-                renderer.positionCount +=1;
-                renderer.SetPosition(renderer.positionCount-1, start_pos);
-                Vector3[] collider_list = new Vector3[renderer.positionCount];
-                Vector2[] collider_list_2 = new Vector2[renderer.positionCount];
-                renderer.GetPositions(collider_list);
-                for (int i = 0; i< renderer.positionCount; i++){
+                //　lineの終点と起点をそろえる
+                line1.loop = true;
+                circle = true;
+
+                Vector3[] collider_list = new Vector3[line1.positionCount];
+                Vector2[] collider_list_2 = new Vector2[line1.positionCount];
+                // lineの各頂点の座標を取得してVector2に変換
+                line1.GetPositions(collider_list);
+                for (int i = 0; i< line1.positionCount; i++){
                     collider_list_2[i]= transform.TransformPoint(collider_list[i]);
                 }
+                // lineと同じ形のコライダーを一瞬だけ作成
                 collider.SetPath(0, collider_list_2);
-                yield return new WaitForSeconds(0.01f);
-                collider.pathCount = 0;
-                break;
+                yield break;
             }
         }
     }
-
+    
     private void OnTriggerEnter2D(Collider2D collider)
     {
         /// 2D同士が接触した瞬間の１回のみ呼び出される処理
-        Destroy(collider.gameObject);
-        fun_counter -= 1;
+        if(collider.gameObject.tag == "Player"){
+            selectObject.Add(collider.gameObject);
+            collider.gameObject.GetComponent<Bacteria>().state = 'S';
+        }
     }
 }
